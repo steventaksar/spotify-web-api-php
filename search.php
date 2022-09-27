d69d12f9a2cb7a543180ee38f4b2ece97b5b92ed
steventaksar <steven@taksar.de>
2022-09-27 16:09:42 +0200

    add search and optimize index

--- search.php
+++ search.php
@@ -1,1 +1,29 @@
 <?php
+$key = $_GET['key'];
+
+$session = new SpotifyWebAPI\Session(
+    'c4ba264bb5164e8f86e5d222f4a6f504',
+    '54741742278047aaa9b05d141ae1aae3',
+    'https://spotify.silvesterintegel.de'
+);
+
+$api = new SpotifyWebAPI\SpotifyWebAPI();
+
+if (isset($_GET['code'])) {
+    $session->requestAccessToken($_GET['code']);
+    $api->setAccessToken($session->getAccessToken());
+
+    print_r($api->me());
+} else {
+    $options = [
+        'scope' => [
+            'user-read-email',
+        ],
+    ];
+
+    header('Location: ' . $session->getAuthorizeUrl($options));
+    die();
+}
+
+$results = $api->search($key, "track");
+var_dump($results);
